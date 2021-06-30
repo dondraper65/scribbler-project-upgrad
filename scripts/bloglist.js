ac1df3a338ac3af0f93d6e66c772f8dd9f363524
Rahul Singh Raghav <officialrahulsr@gmail.com>
2021-06-30 20:19:31 +0530

    Added bloglist.css and .js files with header functionality.

---
+++ scripts/bloglist.js
@@ -0,0 +1,37 @@
+var signupModal = document.getElementById("signupModal");
+var signupBtn = document.getElementById("signupButton");
+var signupClose = document.getElementsByClassName("close")[0];
+var signupAnchor = document.getElementById("signup-anchor");
+
+signupAnchor.onclick = function(){
+    signinModal.style.display = "none";
+    signupModal.style.display = "block"
+}
+
+signupBtn.onclick = function() {
+    signupModal.style.display = "block";
+}
+
+signupClose.onclick = function(){
+    signupModal.style.display = "none";
+}
+
+
+
+var signinModal = document.getElementById("signinModal");
+var signinBtn = document.getElementById("signinButton"); 
+var signinClose = document.getElementsByClassName("close")[1];
+
+signinBtn.onclick = function(){
+    signinModal.style.display = "block";
+}
+
+signinClose.onclick = function(){
+    signinModal.style.display = "none";
+}
+
+window.onclick = function(){
+    if(event.target === signinModal){
+        signinModal.style.display = "none";
+    }
+}
